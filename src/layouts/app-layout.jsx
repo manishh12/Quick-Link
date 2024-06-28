1dfb0da5a20fd07add04256641d0857eec725466
manishh12 <manishdid360@gmail.com>
2024-06-28 23:19:07 +0530

    Full stack project

---
+++ src/layouts/app-layout.jsx
@@ -0,0 +1,18 @@
+import Header from "@/components/header";
+import {Outlet} from "react-router-dom";
+
+const AppLayout = () => {
+  return (
+    <div>
+      <main className="min-h-screen container">
+        <Header />
+        <Outlet />
+      </main>
+      <div className="p-10 text-center bg-gray-800 mt-10">
+        This is one of thr projects made by me for the learning Purpose.
+      </div>
+    </div>
+  );
+};
+
+export default AppLayout;
